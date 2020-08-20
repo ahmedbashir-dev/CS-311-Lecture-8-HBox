5fa9411a059ca36c4c00662400a6f3db0a1654f3
= <=>
2020-08-20 14:23:28 +0500

    HBox with Spacing and Padding

--- src/hboxdemo/Main.java
+++ src/hboxdemo/Main.java
@@ -2,6 +2,7 @@ package hboxdemo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,16 +13,28 @@ import javafx.stage.Stage;
 public class Main extends Application {
     private HBox root;
     private Button button;
+    private Button[] btnArray;
     private Label label;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        button = new Button(); //Create a new button
+        /*button = new Button(); //Create a new button
         button.setText("Click Me"); //Set the text of the button
-        label = new Label("This is Label"); //Create a label with text specified
+        label = new Label("This is Label"); //Create a label with text specified*/
+        btnArray = new Button[10]; //Create a button type array
 
+        //Creates 10 Buttons
+        for (int i = 0; i < btnArray.length ; i++) {
+            btnArray[i] = new Button("Button " + (i+1));
+//            btnArray[i].setWrapText(true);
+        }
         root = new HBox(); //Create a HBox using No-arg constructor
-        root.getChildren().addAll(label, button); //add child nodes in the hbox
+       // root.getChildren().addAll(label, button); //add child nodes in the hbox
+        //Adds 10 Buttons to the HBox
+        for (int i = 0; i < btnArray.length ; i++) {
+           root.getChildren().add(btnArray[i]);
+        }
         root.setSpacing(20); //set spacing of HBox
+        root.setPadding(new Insets(20));
         Scene scene = new Scene(root,400,200); //Create scene
         primaryStage.setScene(scene); //set the scene on stage
         primaryStage.setTitle("Hello World");
